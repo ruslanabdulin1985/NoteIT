80c3f8b0621d87bc1ddeccecae04620eb6eda07e
Ruslan <ruslanabdulin1985@gmail.com>
2020-12-24 19:20:48 +0100

    mocked databases created, app is runnable

--- db.js
+++ db.js
@@ -1,3 +1,36 @@
+const initialise = () => {
+  // initialise database
+}
+
+const create = () => {
+  // create Note
+}
+
+const auth = () => {
+  // auth 
+}
+
+const find = () => {
+  // find note
+}
+
+
+const findAll = () => {
+  // find all Note
+}
+
+
+const deleteOne = () => {
+  // delete a note
+}
+
+const update = () => {
+
+  // edit a note
+}
+
+
+
 module.exports = {
   init: initialise,
   find: find,
--- users.js
+++ users.js
@@ -1,3 +1,20 @@
+const initialise = () => {
+  // initialise database
+}
+
+const createUsr = () => {
+  // create User
+}
+
+const auth = () => {
+  // auth 
+}
+
+const find = () => {
+  // find database
+}
+
+
 module.exports = {
   init: initialise,
   find: find,
